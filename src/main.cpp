7e125f6c1f9ac20a5d74f5e69789292228d13640
Manfred Ackermann <manfred.ackermann@gmail.com>
2020-05-16 22:58:40 +0200

    Cleanup and compensation added.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 // Slow memory variable assignment
 enum {
-  SLOW_BLINK_STATE,     // Blink status
+  //SLOW_BLINK_STATE,     // Blink status
   SLOW_PROG_ADDR        // Program start address
 };
 
@@ -17,66 +17,64 @@ void ULP_CHARGE_PUMP_DRIVER() {
   memset(RTC_SLOW_MEM, 0, 8192);
 
   // Blink status initialization
-  RTC_SLOW_MEM[SLOW_BLINK_STATE] = 6;
+  //RTC_SLOW_MEM[SLOW_BLINK_STATE] = 6;
 
-  // PumpChannel0 (specify by +14)
-  const int pin_blink_bit1 = RTCIO_GPIO32_CHANNEL + 14;
-  const gpio_num_t pin_blink1 = GPIO_NUM_32;
+  // PumpChannel0
+  const int pump_chan_0 = RTCIO_GPIO32_CHANNEL + 14; // (specify by RTCIO_CHANNEL_9 + 14)
 
-  const int pin_blink_bit2 = RTCIO_GPIO33_CHANNEL + 14;
-  const gpio_num_t pin_blink2 = GPIO_NUM_33;
+  // PumpChannel1
+  const int pump_chan_1 = RTCIO_GPIO33_CHANNEL + 14; // (specify by RTCIO_CHANNEL_8 + 14)
 
   // GPIO2 (LED_BUILTIN) initialization (set to output and initial value is 0)
-  rtc_gpio_init(pin_blink1);
-  rtc_gpio_set_direction(pin_blink1, RTC_GPIO_MODE_OUTPUT_ONLY);
-  rtc_gpio_set_level(pin_blink1, 0);
+  rtc_gpio_init(GPIO_NUM_32);
+  rtc_gpio_set_direction(GPIO_NUM_32, RTC_GPIO_MODE_OUTPUT_ONLY);
+  rtc_gpio_set_level(GPIO_NUM_32, 0);
 
-  rtc_gpio_init(pin_blink2);
-  rtc_gpio_set_direction(pin_blink2, RTC_GPIO_MODE_OUTPUT_ONLY);
-  rtc_gpio_set_level(pin_blink2, 0);
+  rtc_gpio_init(GPIO_NUM_33);
+  rtc_gpio_set_direction(GPIO_NUM_33, RTC_GPIO_MODE_OUTPUT_ONLY);
+  rtc_gpio_set_level(GPIO_NUM_33, 0);
 
   // ULP Program
   const ulp_insn_t  ulp_prog[] = {
+    M_LABEL(97),                            // label_97 => INIT
+    I_MOVI(R1, 7),                          // R1 = Sequence Size: 6
+
     M_LABEL(98),                            // label_98 => START
-    I_MOVI(R3, SLOW_BLINK_STATE),           // R3 = SLOW_BLINK_STATE
-    I_LD(R1, R3, 0),                        // R1 = RTC_SLOW_MEM[R3(SLOW_BLINK_STATE)]
+    I_SUBI(R1, R1, 1),                      // R1 = R1 - 1
 
                                             // => Sequenz 6
     I_ANDI(R0, R1, 6),                      // R0 = R1 AND 6
     M_BL(1,6),                              // IF R0 < 6 THAN GOTO M_LABEL(1)
-    I_WR_REG(RTC_GPIO_OUT_REG, pin_blink_bit1, pin_blink_bit1, 1),// pin_blink_bit = 1
+    I_WR_REG(RTC_GPIO_OUT_REG, pump_chan_0, pump_chan_0, 1),// pump_chan_0 = 1
+    I_DELAY(60),                            // Compensate Sequenz 4/3/1 additional ticks
     M_BX(99),                               // GOTO M_LABEL(99)
 
 
     M_LABEL(1),                             // label_1 => Sequenz 4
     I_ANDI(R0, R1, 4),                      // R0 = R1 AND 4
     M_BL(2,4),                              // IF R0 < 6 THAN GOTO M_LABEL(2)
-    I_WR_REG(RTC_GPIO_OUT_REG, pin_blink_bit1, pin_blink_bit1, 0),// pin_blink_bit = 0
+    I_WR_REG(RTC_GPIO_OUT_REG, pump_chan_0, pump_chan_0, 0),// pump_chan_0 = 0
     M_BX(99),                               // GOTO M_LABEL(99)
 
 
     M_LABEL(2),                             // label_2 => Sequenz 3
     I_ANDI(R0, R1, 3),                      // R0 = R1 AND 3
     M_BL(3,3),                              // IF R0 < 3 THAN GOTO M_LABEL(3)
-    I_WR_REG(RTC_GPIO_OUT_REG, pin_blink_bit2, pin_blink_bit2, 1),// pin_blink_bit = 1
+    I_WR_REG(RTC_GPIO_OUT_REG, pump_chan_1, pump_chan_1, 1),// pump_chan_1 = 1
     M_BX(99),                               // GOTO M_LABEL(99)
 
 
     M_LABEL(3),                             // label_3 => Sequenz 1
     I_ANDI(R0, R1, 1),                      // R0 = R1 AND 1
     M_BL(99,1),                             // IF R0 < 1 THAN GOTO M_LABEL(99)
-    I_WR_REG(RTC_GPIO_OUT_REG, pin_blink_bit2, pin_blink_bit2, 0),// pin_blink_bit = 0
-
+    I_WR_REG(RTC_GPIO_OUT_REG, pump_chan_1, pump_chan_1, 0),// pump_chan_1 = 0
+    I_DELAY(40),                            // Compensate missing M_BX
 
     M_LABEL(99),                            // label_99 => Finish up
     I_SUBI(R0, R1, 1),                      // R0 = R1 - 1
 
-    M_BGE(4, 1),                            // IF R0 >= 1 THAN GOTO M_LABEL(4)
-    I_MOVI(R0, 6),                          // R0 = 6
-
-    M_LABEL(4),                             // M_LABEL(4)
-    I_ST(R0, R3, 0),                        // RTC_SLOW_MEM[R3(SLOW_BLINK_STATE)] = R0
-    M_BX(98)                                // GOTO M_LABEL(98)
+    M_BGE(98, 1),                           // IF R0 >= 1 THAN GOTO M_LABEL(98) START
+    M_BX(97)                                // GOTO M_LABEL(97) INIT
   };
 
   // Run the program shifted backward by the number of variables
@@ -101,7 +99,7 @@ void setup() {
   WiFi.mode(WIFI_OFF);
   btStop();
 
-  esp_deep_sleep_start();
+  //esp_deep_sleep_start();
   while(!Serial){}
   delay(1000);
   ulpDump();
